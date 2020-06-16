bdb60055c31a50db2ea13f246ffe4a5e7ca8123d
HuangZJ0607 <1009183589@qq.com>
2020-06-16 23:39:03 +0800

    modified Mysql.py

--- Common/Mysql.py
+++ Common/Mysql.py
@@ -4,4 +4,4 @@
 # @Time     :2020/6/11 15:38
 import pymysql
 
-con = pymysql.connect(host='', port='', user='', password='')
+con = pymysql.connect(host='', port='', user='root', password='000000')
